b3f5763b18b637e191fb52d2e580a72791914837
Daniel Czerwonk <czerwonk@mauve.de>
2010-01-19 14:23:28 +0100

    JavaDoc for IMessageCreator.java

--- src/net/atomique/ksar/IMessageCreator.java
+++ src/net/atomique/ksar/IMessageCreator.java
@@ -6,11 +6,35 @@ package net.atomique.ksar;
  */
 public interface IMessageCreator {
 
+	/**
+	 * Shows a dialog displaying an info text
+	 * @param title Title to show
+	 * @param message Message to show
+	 */
 	void showInfoMessage(String title, String message);
 	
+	/**
+	 * Shows a dialog to confirm a request
+	 * @param title Title to show
+	 * @param message Message to show
+	 * @return true (yes) oder false (no)
+	 */
 	boolean showConfirmationDialog(String title, String message);
 	
+	/**
+	 * Shows a dialog with a text input field in a suggestion mode
+	 * @param title Title to show
+	 * @param message Message to show
+	 * @param suggestions
+	 * @return Entered text or null (if dialog was canceled)
+	 */
 	String showTextInputWithSuggestionDialog(String title, String message, Iterable<String> suggestions);
 	
+	/**
+	 * Shows a dialog with a text input field
+	 * @param title Title to show
+	 * @param message Message to show
+	 * @return Entered text or null (if dialog was canceled)
+	 */
 	String showTextInputDialog(String title, String message);
 }
